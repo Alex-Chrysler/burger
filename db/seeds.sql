9a35a5560cb07052903e6b96178e00c3a29950a5
achrysler <achrysler36@gmail.com>
2018-10-29 14:02:17 -0500

    FIr commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggi Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
+
